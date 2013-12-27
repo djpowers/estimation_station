1c307e1e9cf64721658d4e0975177be6868023e5
Dave Powers <djpowers89@gmail.com>
2013-12-26 21:32:50 -0500

    Add spec tests and player model.

---
+++ spec/models/player_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe Player do
+
+  it { should have_valid(:name).when("Brian W.", "David") }
+  it { should_not have_valid(:name).when(nil, "") }
+
+  it { should validate_uniqueness_of(:name) }
+
+end
