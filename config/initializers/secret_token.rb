aced841e35d3dd17e1b44576464fdfa16356fec7
Dave Powers <djpowers89@gmail.com>
2013-12-26 21:15:59 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EstimationStation::Application.config.secret_key_base = '25e41716ce0b640ba124a9ebf4f8f30cb46acfe571c9981d9c5f3a74f3bcd2c9feaf7eae756623d0af4d44e65926a46ba5964f1ae2ed415f1f6c63278ad52289'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EstimationStation::Application.config.session_store :cookie_store, key: '_estimation_station_session'
