0c805e10a9a9b38cc30f116d6926694d8ee28c3b
Dave Powers <djpowers89@gmail.com>
2014-02-16 23:17:34 -0500

    Move coveralls to top of spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,11 +4,11 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 
-require 'capybara/rails'
-require 'capybara/rspec'
-
 require 'coveralls'
 Coveralls.wear!
+
+require 'capybara/rails'
+require 'capybara/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
