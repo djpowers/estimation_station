a1f511372cff42772764eca7cac733af2992252e
Dave Powers <djpowers89@gmail.com>
2014-11-18 23:31:24 -0500

    Fix migration to update group_id foreign key columns

--- db/migrate/20140210041819_change_group_to_game.rb
+++ db/migrate/20140210041819_change_group_to_game.rb
@@ -1,5 +1,7 @@
 class ChangeGroupToGame < ActiveRecord::Migration
   def change
     rename_table :groups, :games
+    rename_column :jars, :group_id, :game_id
+    rename_column :players, :group_id, :game_id
   end
 end
